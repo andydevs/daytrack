718dd021315da14feeb4330c1ea225ba3d96dfdb
andydevs <akanshul97@gmail.com>
2021-02-18 22:29:03 -0500

    Added reaaaaaackt

--- app/index.js
+++ app/index.js
@@ -4,3 +4,9 @@
  * Author:  Anshul Kharbanda
  * Created: 2 - 18 - 2021
  */
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = <h1>Hello hello helloooooo</h1>
+
+ReactDOM.render(app, document.querySelector('#app'))
